86fd0ed424fb96e342d4592de345bd7729df5893
Corban <sonnemanc@gmail.com>
2022-03-25 11:53:12 -0700

    commit tree c90f1955c21cbefb3e9c342e4207154f98e394df

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map( (movie) => (
+          <div>
+            <p>{movie.title}</p>
+            <p>{movie.time}</p>
+            <ul>
+              {movie.genres.map( (genre) => (
+                <li>{genre}</li>
+              ) )}
+            </ul>
+          </div>
+        ) )}
     </div>
   );
 };
